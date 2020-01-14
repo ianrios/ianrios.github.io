c6a15579f6c7b0d1a3bfef08c0681d792903f9ee
ianrios <ian.rios1a@gmail.com>
2020-01-14 12:10:50 -0500

    added colors and mainpage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="main">
+			<p className="name">
+				Ian Rios
+			</p>
+			<a
+				className="link"
+				href="#"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				enter
+			</a>
+		</div>
+	);
 }
 
 export default App;
