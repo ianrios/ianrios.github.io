2ed9839c3a378b782c32cad9bac787f6a98ed0f6
ianrios <ian.rios1a@gmail.com>
2020-01-14 16:36:19 -0500

    Updates

---
+++ precache-manifest.9b469b5af4553202c1ff4b42736373e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7698e52cac3ace70ce29f55efcbaadae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f6e656de363d005897",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "700421eafa57091819fd",
+    "url": "/static/css/main.b31c1434.chunk.css"
+  },
+  {
+    "revision": "52f6e656de363d005897",
+    "url": "/static/js/2.ae9ea7a7.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ae9ea7a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "700421eafa57091819fd",
+    "url": "/static/js/main.307b32d3.chunk.js"
+  },
+  {
+    "revision": "3d9a528714d58af39fa7",
+    "url": "/static/js/runtime-main.9632ff6f.js"
+  }
+]);
