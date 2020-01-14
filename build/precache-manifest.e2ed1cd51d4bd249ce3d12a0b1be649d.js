d17fac4151dbab113258a4eebe9b04dddfac2183
ianrios <ian.rios1a@gmail.com>
2020-01-14 16:19:28 -0500

    added old master for reference

---
+++ build/precache-manifest.e2ed1cd51d4bd249ce3d12a0b1be649d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273e03f3408fd3e05b0abe3e9e3b6b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "032d60b03749a90b2898",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "833808b50febf3235cc6",
+    "url": "/static/css/main.b31c1434.chunk.css"
+  },
+  {
+    "revision": "032d60b03749a90b2898",
+    "url": "/static/js/2.b08a5d39.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.b08a5d39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "833808b50febf3235cc6",
+    "url": "/static/js/main.14ef4ba1.chunk.js"
+  },
+  {
+    "revision": "71c3a6bfe973a94c9ef7",
+    "url": "/static/js/runtime-main.ac78dd70.js"
+  }
+]);
