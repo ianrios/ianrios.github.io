8217abd148c9f0550069b6d8af298e13b54d26c8
Ian Rios <ianrios@Ians-MacBook-Pro.local>
2021-11-10 06:53:54 -0500

    added data

--- src/App.js
+++ src/App.js
@@ -1,120 +1,112 @@
 import React, { useState, useEffect } from 'react';
-import { findDOMNode } from "react-dom";
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link
-} from "react-router-dom";
-
-
+// import { Switch, Route, Link } from "react-router-dom";
 import Modal from 'react-bootstrap/Modal'
-
 import './App.css';
+import { data } from './data'
 
 
 
 function App() {
-	const [view, setView] = useState(true);
+
+	const [view, setView] = useState('welcome');
+	useEffect(() => {
+		const lsView = window.localStorage.getItem('view')
+		if (lsView) {
+			setView(lsView)
+		}
+	}, [])
+	useEffect(() => {
+		window.localStorage.setItem('view', view)
+	}, [view])
+
 	const [ul, setUl] = useState(false);
 	const [modalShow, setModalShow] = useState(false);
 
+	const [projectView, setProjectView] = useState(false);
 
 	return (
-		<Router>
+		<>
 
-			{view ? (
-				<div className="view-1">
-					<p className="name">
-						Ian Rios
-				</p>
-					<a
+			{view === 'welcome' && <div className="view-1">
+				<p className="name">Ian Rios</p>
+				<p className='text-center'>
+					<button
 						className="open-link"
-						href="#"
-						rel="noopener noreferrer"
-						onClick={() => setView(false)}
-					>
-						enter
-				</a>
-				</div>
-			) : (
-					<div className="view-2">
-						<div className="sidebar">
-							<div id="header">
-								<h1>Ian Rios</h1>
-								<div id="expandButton"></div>
-							</div>
-
-							<a className="h2-size first-h2" href="https://github.com/ianrios">
-								projects
-								</a>
-
-							<a onClick={() => setUl(!ul)} className="h2-size ul-show" href="#">
-								social
-							</a>
-
-							<ul style={{ display: ul ? "block" : "none" }}>
-								<li><a className="flat-link" target="_blank" href="https://github.com/ianrios/">github</a></li>
-								<li><a className="flat-link" target="_blank" href="https://www.linkedin.com/in/ian-rios/">linkedin</a></li>
-								<li><a className="flat-link" target="_blank" href="https://twitter.com/ian_rios_">twitter</a></li>
-								<li><a className="flat-link" target="_blank" href="https://www.codewars.com/users/ianrios">codewars</a></li>
-							</ul>
-
-
-
-							<a href="#" className="h2-size" onClick={() => setModalShow(true)}>contact</a>
-							<MyVerticallyCenteredModal
-								show={modalShow}
-								onHide={() => setModalShow(false)}
-							/>
-							<a
-								className="h2-size back-link"
-								onClick={() => setView(view - 1)}
-								href="#"
-							>
-								back
-							</a>
+						onClick={() => setView('main')}
+					>enter</button>
+				</p>
+			</div>
+			}
+			{view === 'main' &&
+				<div className="view-2">
+					<div className="sidebar">
+						<div id="header">
+							<h1>Ian Rios</h1>
+							<div id="expandButton"></div>
 						</div>
 
-						<div className="grid">
-							<div className="row">
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/bandreader.png" />
-								</div>
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/borilliant_fb.png" />
-								</div>
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/recorder.png" />
-								</div>
-							</div>
-							<div className="row">
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/fallingnotes.png" />
-								</div>
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/interface_JS_42218.png" />
-								</div>
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/midireceive.png" />
-								</div>
-							</div>
-							<div className="row">
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/momsearch.png" />
-								</div>
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/score_reader.png" />
-								</div>
-								<div className="col img-wrap">
-									<img className="img-fluid img-t" src="img/V1.png" />
+						<button className="h2-size first-h2" onClick={() => setProjectView(true)}>projects</button>
+
+						<button onClick={() => setUl(!ul)} className="h2-size ul-show" >social</button>
+						<ul style={{ display: ul ? "block" : "none" }}>
+							<li><a className="flat-link" target="_blank" href="https://github.com/ianrios/">github</a></li>
+							<li><a className="flat-link" target="_blank" href="https://www.linkedin.com/in/ian-rios/">linkedin</a></li>
+							<li><a className="flat-link" target="_blank" href="https://twitter.com/ian_rios_">twitter</a></li>
+							<li><a className="flat-link" target="_blank" href="https://www.codewars.com/users/ianrios">codewars</a></li>
+						</ul>
+
+
+						<button className="h2-size" onClick={() => setModalShow(true)}>contact</button>
+						<MyVerticallyCenteredModal
+							show={modalShow}
+							onHide={() => setModalShow(false)}
+						/>
+						<button
+							className="h2-size back-link"
+							onClick={() => setView('welcome')}
+
+						>back</button>
+					</div>
+					{/* <Switch>
+						<Route to>
+
+						</Route>
+					</Switch> */}
+					<p className='text-center'>
+						<h3 className='pt-5'>Projects</h3>
+					</p>
+					<div className='container'>
+						<div className='row'>
+							<div className='col-11 offset-1'>
+								{/* <div class="card-group"> */}
+								<div className="row row-cols-md-1 row-cols-lg-3"
+
+								// data-masonry='{"percentPosition": true }'
+								>
+									{data.map((item, index) => {
+										return (
+											<div className='col' key={index}>
+												<div className="card m-2" >
+													<div className="card-body">
+														<h5 className="card-title">{item.title}</h5>
+														<img className="card-img-top" src={item.img_src} alt="Card image cap" />
+														<p className="card-text">{item.body}</p>
+														{item.href.length > 0 && <a href={item.href} target="_blank" className="btn btn-secondary">Visit GitHub Repo</a>}
+														{item.live.length > 0 && <a href={item.live} target="_blank" className="btn btn-succcess">Visit Live Demo</a>}
+													</div>
+												</div>
+											</div>
+										)
+									})}
+									{/* </div> */}
 								</div>
 							</div>
 						</div>
 					</div>
-				)
+
+				</div>
 			}
-		</Router >
+		</>
 	);
 }
 
@@ -143,7 +135,7 @@ function MyVerticallyCenteredModal(props) {
 
 			</Modal.Body>
 			<Modal.Footer>
-				<a onClick={props.onHide}>Close</a>
+				<button className="btn btn-secondary" onClick={props.onHide}>Close</button>
 			</Modal.Footer>
 		</Modal>
 	);
---
+++ src/data.js
@@ -0,0 +1,87 @@
+export const data = [
+    {
+        title: "WHY? Record Company Website",
+        img_src: "./",
+        body: "",
+        href: ""
+    },
+    {
+        title: "The Algorithm Knows Best",
+        img_src: "./",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Top-5",
+        img_src: "./",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Twitterbot",
+        img_src: "./",
+        body: "",
+        href: ""
+    },
+    {
+        title: "PLant lOG",
+        img_src: "./",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Borilliant",
+        img_src: "./img/borilliant_fb.png",
+        body: "",
+        href: ""
+    },
+    {
+        title: "neThing",
+        img_src: "./img/interface_JS_42218.png",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Barely Enough Ingredients - Many Online Meals",
+        img_src: "./img/momsearch.png",
+        body: "The Many Online Meals (MOM) app is a ruby on rails web app that utilizes bootstrap for styling, the Spoonacular API for recipe and ingredient data, and heroku for hosting.",
+        href: "https://github.com/ianrios/BarelyEnoughIngredients/tree/master",
+        live:"https://bei-mom.herokuapp.com/"
+    },
+    {
+        title: "RAAPT V1",
+        img_src: "./img/V1.png",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Score Reader",
+        img_src: "./img/score_reader.png",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Falling Notes",
+        img_src: "./img/fallingnotes.png",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Band Reader",
+        img_src: "./img/bandreader.png",
+        body: "",
+        href: ""
+    },
+    {
+        title: "Recorder",
+        img_src: "./img/recorder.png",
+        body: "",
+        href: ""
+    },
+    {
+        title: "MIDI Recieve UDP",
+        img_src: "./img/midireceive.png",
+        body: "",
+        href: ""
+    },
+]
