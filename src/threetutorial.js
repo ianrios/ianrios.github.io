d3a6497358d9567ab3d3411bbede2b689aa1f04c
Ian Rios <ianrios@Ians-MacBook-Pro.local>
2021-11-10 19:26:38 -0500

    save point

--- src/threetutorial.js
+++ src/threetutorial.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
+import {useBox} from ''
 import './three.css'
 
 function Box() {
